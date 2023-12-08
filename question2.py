035a329237a1530c2b016341f3271f57b0273e1a
Ivan-Zadorozhnyy <119039373+Ivan-Zadorozhnyy@users.noreply.github.com>
2023-12-08 23:16:36 +0200

    question 2 as separate file

---
+++ question2.py
@@ -0,0 +1,28 @@
+import csv
+import matplotlib.pyplot as plt
+
+battle_data = []
+with open("ClashRoyaleData.csv", "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        battle_data.append(row)
+
+elixir_level_differences = []
+
+for battle in battle_data:
+    if int(battle["totalDamage"]) > 12000:
+        winner_elixir = float(battle["winnerElixir"])
+        loser_elixir = float(battle["loserElixir"])
+        diff = abs(winner_elixir - loser_elixir)
+        elixir_level_differences.append(diff)
+
+bin_edges = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
+             1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0]
+
+plt.figure(figsize=(10, 6))
+plt.hist(elixir_level_differences, bins=bin_edges, color='skyblue', edgecolor='black')
+plt.title("Elixir Level Differences in High-Damage Battles")
+plt.xlabel("Elixir Level Difference")
+plt.ylabel("Frequency")
+plt.grid(True)
+plt.show()
